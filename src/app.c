73540e6f4460281ab44162314582b19fee1a1ad1
zedzee <carsonuderitz@gmail.com>
2024-10-22 11:00:37 -0400

    Window

--- src/app.c
+++ src/app.c
@@ -1,4 +1,5 @@
 #include "app.h"
+#include <GLFW/glfw3.h>
 
 RayError appRun() {
 	DEFINE_ERR();
@@ -10,23 +11,48 @@ RayError appRun() {
 	error = appLoop(&app);
 	CHECK_ERROR(error);
 
-	error = appCleanup(&app);
-	CHECK_ERROR(error);
-
+	appCleanup(&app);
 	return error;
 }
 
 RayError appInit(App *app) {
 	DEFINE_ERR();
+
+	app->window = appInitGlfw();
+
+	error = appCreateInstance(&app->instance);
+	CHECK_ERROR(error);
+
 	return error;
 }
 
 RayError appLoop(App *app) {
 	DEFINE_ERR();
+
+	while (true) {
+		glfwPollEvents();
+	}
+
 	return error;
 }
 
-RayError appCleanup(App *app) {
+void appCleanup(App *app) {
+	glfwDestroyWindow(app->window);
+}
+
+GLFWwindow *appInitGlfw() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow *window;
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", NULL, NULL);
+	return window;
+}
+
+RayError appCreateInstance(VkInstance *instance) {
 	DEFINE_ERR();
+
 	return error;
 }
--- src/app.h
+++ src/app.h
@@ -2,11 +2,18 @@
 #include "core_types.h"
 #include <GLFW/glfw3.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+
 typedef struct App {
+	GLFWwindow *window;
 	VkInstance instance;
 } App;
 
 extern RayError appRun();
 extern RayError appInit(App *app);
 extern RayError appLoop(App *app);
-extern RayError appCleanup(App *app);
+extern void appCleanup(App *app);
+
+extern GLFWwindow *appInitGlfw();
+extern RayError appCreateInstance(VkInstance *instance);
